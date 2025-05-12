5b91c472144871f1c3e9f39fa6bc922f605550dc
VIVEK <vivekkumarsharma2663@gmail.com>
2025-05-12 17:52:28 +0000

    Strings basics

---
+++ basics_01/06_dt.js
@@ -0,0 +1,38 @@
+//  const name="Vivek";
+
+// const count=20;
+
+// // console.log(name+count+"Hello");
+
+// console.log(`Hello my name is ${name} and my age is ${count}`) //String Interpolation
+
+const gamename= new String("Vivek-sharma");//String declaration
+
+// console.log(gamename[0]);
+
+// console.log(gamename.length)
+
+// console.log(gamename.toUpperCase())
+
+// console.log(gamename.charAt(0));
+
+// console.log(gamename.indexOf('V'));
+
+// const newname = gamename.substring(0,4);
+// console.log(newname);
+
+// const anotherstring = gamename.slice(0,4);
+// console.log(anotherstring);
+
+const newstring= "  Vivek";
+console.log(newstring);
+console.log(newstring.trim());
+
+
+const url="https-//vivek-com";
+
+console.log(url.replace('https','xyz'));
+
+console.log(url.includes('vivek'));
+
+console.log(url.split('-'));
