a74730f1c7b08b687a3fa173ead29c217e0165e3
VIVEK <vivekkumarsharma2663@gmail.com>
2025-05-12 17:18:51 +0000

    Datatype summary

---
+++ basics_01/05_dt.js
@@ -0,0 +1,16 @@
+//Primitive Data type
+
+// 7 types: String , Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference (Non-Primitive)
+
+// Array ,Objects, Functions
+
+// Javascript is a dynamically typed language
+
+const id= Symbol('123');
+const anotherid = Symbol('123');
+console.log(id);
+console.log(anotherid);
+console.log(id==anotherid);
+
