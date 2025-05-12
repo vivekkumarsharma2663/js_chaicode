35b8b7ed9770da0c910a00615e4dfca205cebdef
VIVEK <vivekkumarsharma2663@gmail.com>
2025-05-12 16:48:10 +0000

    Basics Operations

--- basics_01/03_dt.js
+++ basics_01/03_dt.js
@@ -18,4 +18,36 @@ let booleanIsLoggedIn=Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn)
 */
 
+//*************OPERATIONS***********//
 
+// let value=3;
+// let negValue =-value;
+// console.log(value);
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%2);
+
+// let str1="Hello";
+// let str2="Vivek";
+// let str3=str1+str2;
+// console.log(str3);
+// console.log("1"+2);
+// console.log("2"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3;
+
+// num1=num2=num3=2+2;
+// console.table([num1,num2,num3]);
+
+let gameCounter =100;
+gameCounter++;
+console.log(gameCounter);
