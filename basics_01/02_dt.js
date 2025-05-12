d8ad1e5c69dc45cf1cafd1f580c2c5cc049e39de
VIVEK <vivekkumarsharma2663@gmail.com>
2025-05-12 16:30:02 +0000

    Datatypes and conversion

---
+++ basics_01/02_dt.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(2+2) //We are using node JS not browser.
+
+//code readability should be done.
+
+let name="Vivek"
+let age=23
+let isLoggedIn=false;
+
+
+// 1.number=> 2 to power 53;
+// 2.bigint
+// 3.string=> "";
+// 4. boolean=> true/false;
+// null
+// undefined
+// symbol => uniqueness
+
+// object 
+
+console.log(typeof undefined);
---
+++ basics_01/03_dt.js
@@ -0,0 +1,21 @@
+/*let score=null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let value=Number(score);
+console.log(typeof value);
+console.log(value);
+
+//"33" =>33
+//"33abc"=>NaN (Not a number)
+// true => 1 false=>0
+//undefined =>NaN
+//null =>0
+
+let isLoggedIn= 1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+*/
+
+
