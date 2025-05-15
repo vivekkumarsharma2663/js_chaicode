76f24eb68b21b274ddede98879d7f244033a3572
VIVEK <vivekkumarsharma2663@gmail.com>
2025-05-15 19:55:39 +0000

    Numbers in JS

---
+++ basics_01/07_dt.js
@@ -0,0 +1,26 @@
+// const score=400
+// console.log(score);
+
+// const balance=new Number(100);
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const othernum=123.8455
+
+// console.log(othernum.toPrecision(4));
+
+// const hundreds=100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//**********************MATHS ****************//
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+console.log(Math.floor(Math.random()*10)+1);
+
